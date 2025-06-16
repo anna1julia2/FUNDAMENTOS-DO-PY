e95b8faded7fd4728f2274b9c20f45d3f2c6a7b5
juju <aj.ncorrea@gmail.com>
2025-06-16 11:46:10 -0300

    Add files via upload

---
+++ AULA-4-contas-praticar.py
@@ -0,0 +1,8 @@
+#% resultado do resto da divisao - (/) modulo
+#//
+#print(14%4) = 2 estudar
+#unario = um operando .. binario = dois operando .. operadores
+#unario (-5 +3) -2 estudar ..
+#binario
+# esquerda pra direita ( 
+#direita pra esquerda (2 ** 2 ** 3)

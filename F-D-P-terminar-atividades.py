55834b834ae3b3beeeda4d9299bf9b8926436809
juju <aj.ncorrea@gmail.com>
2025-06-18 11:14:55 -0300

    Add files via upload

---
+++ F-D-P-terminar-atividades.py
@@ -0,0 +1,77 @@
+#1
+var  =  10
+
+if var % 2 == 0:
+    print('par')
+else:
+    print('Impar')
+    
+#------------------------------------------------
+#2
+n = 10
+n = 15
+
+if n > 10:
+    print('O número é positivo e igual 10')
+if n > 15:
+    print('O número é positivo e maior que 10')    
+    
+else:
+    print('Não é nada')
+    
+#--------------------------------------------------
+#3    
+idade  = 18
+
+if idade >= 16:
+    print('Pode votar')
+else:
+    print('Não pode votar')
+    
+#-----------------------------------------------------
+#4   
+   
+   
+   
+   
+   
+   
+   
+#---------------------------------------------------  
+#5   
+#login_nome = input('Digite seu nome >> ')
+#senha_acesso = input('Digite sua senha >> ')
+
+#if login_nome and senha_acesso ['senhas']:
+#    print('acesso concedido')
+    
+#----------------------------------------------------    
+#6
+    
+    
+    
+    
+    
+    
+    
+#------------------------------------------------   
+#7    
+l1 = 1
+l2 = 2
+l3 = 3
+
+if l1 == l2 == l3 == l1:
+    print('equilatero lados iguais')
+elif l1 != l2 != l3 != l1:
+    print('isocesles lados iguais')
+else: 
+    print('escaleno')
+    
+#---------------------------------------------------------
+#8
+
+
+    
+    
+    
+    

3417085ff2643d94f19308c2d70719fa0c374c0b
juju <aj.ncorrea@gmail.com>
2025-06-18 11:13:25 -0300

    Add files via upload

---
+++ F-D-P-corrigir-atividade.py
@@ -0,0 +1,13 @@
+idade = 18
+tem_carteira = True
+nao_caretira = True
+
+if idade >= 18 and tem_carteira or nao_carteira:
+    print('pode dirigir')
+elif idade >= 18 and tem_carteira or nao_carteira:
+        print('nao pode dirigir')
+elif idade <= 18 and nao_carteira:
+    print('nao_idade')
+        
+else:
+            print('nao tem idade')
